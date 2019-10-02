8a66eaf9ba01f2fe60171e314e0c5e3b6006d7ef
tjharo <orvis952002@yahoo.com>
2019-10-02 03:05:39 +0000

    Database setup

---
+++ db/migrate/20191002025707_create_places.rb
@@ -0,0 +1,10 @@
+class CreatePlaces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.text :description
+      t.string :adress
+      t.timestamps
+    end
+  end
+end
